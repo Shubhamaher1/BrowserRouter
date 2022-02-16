3ebc3950377b66bc1a02b3971bb3dad586faa66a
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-17 01:06:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import{BrowserRouter,Link,Switch,Route,Routes} from "react-router-dom";
+import { About } from './Component/About';
+import { Home } from './Component/Home';
+import { User } from './Component/User';
+import { LandingPage } from './Component/LandingPage';
+import { LogIn } from './Component/LogIn';
+import { SignUp } from './Component/SignUp';
+import { Thanks } from './Component/Thanks';
+
+// import React from 'react'
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}></Route>
+      <Route path='/log-in' element={<LogIn/>}></Route>
+      <Route path='/sin-in' element={<SignUp/>}></Route>
+      <Route path='/welcome' element={<Thanks/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+//link for link
+// const App = () => {
+//   return (
+//     <Router>
+//     {/* wrap the the BrowserRowter */}
+//     <div className='header'>
+//      <nav>
+//      <ul>
+//         <li><Link to='/'>Home</Link> </li>
+//         <li><Link to='/about'>About</Link> </li>
+//         <li><Link to='/username'>Username</Link> </li>
+//       </ul>
+//      </nav>
+     
+    
+//       <Routes>
+//       <Route path='/about' element={<About/>}></Route>
+//       <Route path='/' element={<Home/>}></Route>
+//       <Route path='/username' element={<User/>}></Route>
+     
+//     </Routes>
+//     </div>
+//     </Router>
+    
+//   )
+// }
+export default App;
